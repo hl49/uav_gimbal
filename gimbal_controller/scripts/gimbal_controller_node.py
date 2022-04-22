621265c50e68585e6216129eedc46b32c8901580
hl49 <henrylema94@gmail.com>
2022-04-22 13:15:00 -0700

    Generation and use of RPYAxes.msg

--- gimbal_controller/scripts/gimbal_controller_node.py
+++ gimbal_controller/scripts/gimbal_controller_node.py
@@ -21,9 +21,11 @@ class Gimbal:
         self.servo_data_seq_counter = 0
         self.tilt = 0.0
         self.roll = 0.0
-        self.ref_pos = {'tilt':0.0, 'roll': 0.0} # From perception: input
-        self.target_pos = {'tilt':0.0, 'roll': 0.0} # Command we send to servos: output
-        self.ini_pos = {'tilt':0.0, 'roll':0.0} # 
+        # Angles roll, pitch and yaw are renamed to
+        # roll, tilt, pan.
+        self.ref_pos = {'roll': 0.0, 'tilt':0.0} # From perception: input
+        self.target_pos = {'roll': 0.0, 'tilt':0.0} # Command we send to servos: output
+        self.ini_pos = {'roll':0.0, 'tilt':0.0} # 
         self.tilt_limit_max = math.radians(45)
         self.tilt_limit_min = math.radians(-45)
         self.roll_limit_max = math.radians(45)
@@ -33,19 +35,12 @@ class Gimbal:
         self.ini_tilt_arr = np.zeros([self.num_samples_init, 1])
         self.ini_roll_arr = np.zeros([self.num_samples_init, 1])
 
-        # self.max_tilt = math.radians(65)
-        # self.max_roll = math.radians(45)
 
         # Init ROS node
         rospy.init_node("gimbal_controller")
 
 
         # Create topics 
-
-        # self.sub_target = rospy.Subscriber("/Imu_data",
-        #         Imu,
-        #         self._get_target_callback,
-        #         queue_size=1)
 
         self.servo_pub = rospy.Publisher("/desired_joint_states",
                     JointState,
@@ -63,6 +58,9 @@ class Gimbal:
 
 
     def get_ini_joint_states_callback(self, data):
+        """ Get initial position of servos and calculate the mean value 
+            to stablish the general initial position
+        """
         if self.count_init_servo_pos < self.num_samples_init:
             joints = dict(zip(data.name, data.position))
             self.ini_tilt_arr[self.count_init_servo_pos, 0] = joints['tilt']
@@ -77,15 +75,30 @@ class Gimbal:
     
 
     def get_perception_data_callback(self, data):
-        # print(data.data)
-        # self.tilt = round(data.data[0],2) * -1.0
-        # self.roll = round(data.data[1],2) * -1.0
+        """ Read reference position vector (ref_pos) from topic
+            and check they are within a desired range.
+        """
+        self.ref_pos['tilt'] = data.norm_vect[0]
+        self.ref_pos['roll'] = data.norm_vect[1]
 
-        pass
+        if self.ref_pos['tilt'] > self.tilt_limit_max:
+            self.ref_pos['tilt'] = self.tilt_limit_max
+        elif self.ref_pos['tilt'] < self.tilt_limit_min:
+            self.ref_pos['tilt'] = self.tilt_limit_min
 
+        if self.ref_pos['roll'] > self.roll_limit_max:
+            self.ref_pos['roll'] = self.roll_limit_max
+        elif self.ref_pos['roll'] < self.roll_limit_min:
+            self.ref_pos['roll'] = self.roll_limit_min
+        
+        
+        print('roll: ', self.ref_pos['roll'])
+        print('tilt', self.ref_pos['tilt'])
         # if self.tilt > - self.max_tilt and self.tilt <
 
     def pub_servos_pos(self):
+        """ Publish servo position to topic read by the driver
+        """
 
         servo_data = JointState()
 
@@ -101,8 +114,9 @@ class Gimbal:
 
 
     def stabilize(self):
-
-        # if there is not target position, set 0. 
+        """Main loop to calculate the target position, controll these values are
+           within the range, and publish the target positions to the driver.
+        """
 
         # Controller
         
